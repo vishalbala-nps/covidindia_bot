00a3ddba57922f9fb81cb93c425ca754c97fe75c
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-04-10 18:33:25 +0530

    Testing..

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import *
+from flask_assistant import Assistant, ask, tell
+
 
 app = Flask(__name__)
+assist = Assistant(app, route='/')
 
 @app.route("/")
 def root():
     return "<h1>Hi!</h1>"
+
+@assist.action('greeting')
+def greet_and_start():
+    return ask("Hello there! Welcome!")
 
 if __name__ == "__main__":
     app.run(port=8080)
